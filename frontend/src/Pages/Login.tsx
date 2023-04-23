ac69836e2575e4bf0e9e4a571760437b9c3c7c1c
042Vidhi <98405549+042Vidhi@users.noreply.github.com>
2023-04-23 18:09:14 +0530

    login

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -10,11 +10,11 @@ const Login = (props: Props) => {
   };
 
   return (
-    <div className="bg-back font-montserrat h-screen">
-        <div className="login-container rounded-lg border-2 border-black-500 flex-col bg-primary items-center">
+    <div className="bg-back object-cover relative font-montserrat h-screen">
+        <div className="login-container absolute  top-8 right-1/2  translate-x-1/2 p-5 rounded-xl border-2 border-black-500 flex-col bg-primary items-center">
         <h1 className="text-3xl text-senary font-bold text-center my-4">Welcome Back</h1>
-        <h2 className="text-center text-senary font-bold my-4">Enter your e-mail and password to login</h2>
-            <form onSubmit={handleSubmit} method="post" className="m-4">
+        <h2 className="text-center text-senary font-bold my-4 mx-5">Enter your e-mail and password to login</h2>
+            <form onSubmit={handleSubmit} method="post" className="m-4 flex-col">
                 <label className="text-senary" >Email</label> 
                 <input type="text" name="email" id="email"  className="p-4 border-2 border-black-500 rounded-lg w-full my-4"
                 placeholder="Your e-mail address"/>
